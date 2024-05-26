147ec5da2bbdd1d8327997c98c575792e905f738
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-26 21:29:27 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 function App() {
+
+  const [pepperoni, setPepperoni] = useState(false);
+
+  function togglePepperoni(e) {
+    setPepperoni(e.target.checked);
+  }
   return (
-    <div></div>
+    <div>
+      <h1>Select pizza Toppings</h1>
+      <input 
+      type="checkbox"  
+      id="pepperoni" 
+      checked={pepperoni}
+      aria-checked={pepperoni}
+      onChange={togglePepperoni}
+      />
+      <label htmlFor="pepperoni">Add Pepperoni</label>
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoni? <li>Pepperoni</li> : null}
+      </ul>
+
+    </div>
   )
 }
 
